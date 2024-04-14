826e02052ae25cfc19dae6be01744f51404edb5c
James Conlon <james.conlon@es.fcm.travel>
2024-04-14 17:07:03 +0200

    change from table to gridview

--- src/components/crossword-view.tsx
+++ src/components/crossword-view.tsx
@@ -11,22 +11,26 @@ type CrosswordViewProps = {
 
 const CrosswordView = ({ cols, rows, grid }: CrosswordViewProps) => {
 	const gridChunked = chunk(grid, cols);
-	const tableElements = gridChunked.map((row, i) => {
-		return (
-			<tr className="border" key={`crossword-row-${i}`}>
-				{row.map((col, j) => (
-					<LetterBox
-						key={`crossword-row-${i}-col${j}`}
-						letter={col}
-					/>
-				))}
-			</tr>
-		);
-	});
+	// const tableElements = gridChunked.map((row, i) => {
+	// 	return (
+	// 		<tr className="border" key={`crossword-row-${i}`}>
+	// 			{row.map((col, j) => (
+	// 				<LetterBox
+	// 					key={`crossword-row-${i}-col${j}`}
+	// 					letter={col}
+	// 				/>
+	// 			))}
+	// 		</tr>
+	// 	);
+	// });
+
+	const tableElements = grid.map((x, i) => (
+		<LetterBox key={`crossword-element-${i}`} letter={x} />
+	));
 
 	return (
 		<>
-			<table className="border">{tableElements}</table>
+			<div className="grid grid-cols-12">{tableElements}</div>
 		</>
 	);
 };
--- src/components/letter-box.tsx
+++ src/components/letter-box.tsx
@@ -6,7 +6,13 @@ type LetterBoxProps = {
 
 const LetterBox = ({ letter }: LetterBoxProps) => {
 	const blackout = letter === '.' ? 'bg-black' : '';
-	return <td className={`border ${blackout}`}>{letter}</td>;
+	return (
+		<div
+			className={`w-10 h-10 flex items-center justify-center border ${blackout}`}
+		>
+			{letter}
+		</div>
+	);
 };
 
 export default LetterBox;
